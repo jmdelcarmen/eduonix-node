70c31ff584c2133308e1a678383f1a8b54be4cb6
Marco <jmdc@my.codergv.com>
2016-11-13 15:32:02 -0600

    started project05

---
+++ project05/README.md
@@ -0,0 +1,25 @@
+# We moved!
+
+[Aqua](https://github.com/jedireza/aqua) is what you're looking for.
+
+Also take a look at [Frame](https://github.com/jedireza/frame), which is just
+the restful API parts of Aqua, bring your own front-end.
+
+
+## Why the change?
+
+Drywall started as my first Node.js project. It was published on [Dec 20th,
+2012](https://github.com/jedireza/drywall/tree/ac07c05c146ca52c9e26d4d60c63052364211087).
+I've learned a lot since then, especially about testing, which Drywall never
+got. And (at the time of writing) I'm a bigger fan of
+[hapi](http://hapijs.com/) than [express](http://expressjs.com/).
+
+
+## Thank you
+
+Thank you so much for your interest in my projects.
+
+
+## Where's the old README?
+
+[Right over here](README-original.md)
---
+++ project05/views/events/index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+exports.find = function(req, res, next){
+  req.query.name = req.query.name ? req.query.name : '';
+  req.query.limit = req.query.limit ? parseInt(req.query.limit, null) : 20;
+  req.query.page = req.query.page ? parseInt(req.query.page, null) : 1;
+  req.query.sort = req.query.sort ? req.query.sort : '_id';
+
+  var filters = {};
+  if (req.query.username) {
+    filters.username = new RegExp('^.*?'+ req.query.username +'.*$', 'i');
+  }
+
+  req.app.db.models.Event.pagedFind({
+    filters: filters,
+    keys: 'name username description',
+    limit: req.query.limit,
+    page: req.query.page,
+    sort: req.query.sort
+  }, function(err, results) {
+    if (err) {
+      return next(err);
+    }
+
+    if (req.xhr) {
+      res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+      results.filters = req.query;
+      res.send(results);
+    }
+    else {
+      results.filters = req.query;
+      res.render('events/index', { events: results.data });
+    }
+  });
+};
+
+exports.read = function(req, res, next){
+  req.app.db.models.Event.findById(req.params.id).exec(function (e, event) {
+    if (e) {
+      return next(e);
+    }
+    if (req.xhr) {
+      res.send(event);
+    }
+    else {
+      res.render('events/details', {event: event});
+    }
+  });
+}
