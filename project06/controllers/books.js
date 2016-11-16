6f87e0bca06186be2fa6a3c5d5b6dd27cf2d0b0e
Marco <jmdc@my.codergv.com>
2016-11-16 16:01:57 -0600

    layout setup

---
+++ project06/controllers/books.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function (router) {
+
+  router.get('/', function (req, res) {
+    res.render('index');
+  });
+
+  router.get('/details/:id', function (req, res) {
+    res.render('books/details');
+  });
+
+}
---
+++ project06/controllers/manage.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = function (router) {
+  
+  router.get('/', function (req, res) {
+    res.render('manage/index');
+  });
+
+  router.get('/books', function (req, res) {
+    res.render('manage/books/index');
+  });
+
+  router.get('/categories', function (req, res) {
+    res.render('manage/categories/index');
+  });
+
+}
