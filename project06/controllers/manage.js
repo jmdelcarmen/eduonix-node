ba23ba8f3ca8852e3e033a2c3c4ef29799dfce95
Marco <jmdc@my.codergv.com>
2016-11-19 13:09:04 -0600

    CRUD to books and categories

--- project06/controllers/manage.js
+++ project06/controllers/manage.js
@@ -9,6 +9,8 @@ module.exports = function (router) {
     res.render('manage/index');
   });
 
+  
+  ///////////////BOOK CRUD/////////////////////
   router.get('/books', function (req, res) {
     Book.find({}, function (err, books) {
       if (err) {
@@ -31,7 +33,7 @@ module.exports = function (router) {
 
   router.post('/books', function (req, res) {
     if (isNaN(req.body.price)) {
-      req.flash('error', 'Price must be a number.');
+      // req.flash('error', 'Price must be a number.');
       res.redirect('/manage/books/add');
     }
 
@@ -49,14 +51,103 @@ module.exports = function (router) {
       if (err) {
         console.log(err);
       }
-      req.flash('success', "Book Added");
+      // req.flash('success', "Book Added");
     });
       res.redirect('/manage/books');
   });
 
+  router.get('/books/edit/:id', function (req, res) {
+    Category.find({}, function (err, categories) {
+      Book.findOne({_id: req.params.id}, function (err, book) {
+        if (err) {
+          console.log(err);
+        }
+        res.render('manage/books/edit', {book: book, categories: categories});
+      });
+    });
+  });
 
+  router.post('/books/edit/:id', function (req, res) {
+    if (isNaN(req.body.price)) {
+      // req.flash('error', 'Price must be a number.');
+      res.redirect('/manage/books/add');
+    }
+
+    Book.update({_id: req.params.id}, {
+      title: req.body.title.trim(),
+      category: req.body.category.trim(),
+      description: req.body.description.trim(),
+      author: req.body.author.trim(),
+      publisher: req.body.publisher.trim(),
+      cover: req.body.cover.trim(),
+      price: req.body.price.trim()
+    }, function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+      // req.flash('success', "Book Updated");
+      res.redirect('/manage/books');
+  });
+
+  router.get('/books/delete/:id', function (req, res) {
+    Book.findByIdAndRemove(req.params.id, function (err) {
+      if (err) {
+        console.log(err);
+      }
+      // req.flash('success', "Book deleted");
+      res.redirect('/manage/books');
+    });
+  });
+
+///////////////CATEGORY CRUD/////////////////////
   router.get('/categories', function (req, res) {
-    res.render('manage/categories/index');
+    Category.find({}, function (err, categories) {
+      if (err) {
+        console.log(err);
+      }
+      res.render('manage/categories/index', {categories: categories});
+    });
+  });
+
+  router.get('/categories/add', function(req,res) {
+    res.render('manage/categories/add');
+  });
+
+  router.get('/categories/delete/:id', function(req,res) {
+    Category.findByIdAndRemove(req.params.id, function (err) {
+      if (err) {
+        console.log(err);
+      }
+      res.redirect('/manage/categories');
+    });
+  });
+
+  router.get('/categories/edit/:id', function(req,res) {
+    Category.findById(req.params.id, function (err, category) {
+      if (err) {
+        console.log(err);
+      }
+      res.render('manage/categories/edit', {category: category});
+    });
+  });
+
+  router.post('/categories/edit/:id', function (req, res) {
+    Category.findByIdAndUpdate(req.params.id, {name: req.body.name}, function (err) {
+      if (err) {
+        console.log(err);
+      }
+      res.redirect('/manage/categories');
+    });
+  })
+
+  router.post('/categories/add', function(req,res) {
+    new Category({name: req.body.name}).save(function (err) {
+      if (err) {
+        console.log(err);
+      }
+    res.redirect('/manage/categories');
+    });
   });
 
 }
--- project06/index.js
+++ project06/index.js
@@ -35,5 +35,3 @@ app.on('start', function () {
     console.log('Application ready to serve requests.');
     console.log('Environment: %s', app.kraken.get('env:env'));
 });
-
-console.log(lusca.key);
